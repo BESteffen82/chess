a514f43480df5cfb3d3ae97be6077b2300e2f4a0
Brent Steffen <BESteffen82@gmail.com>
2022-08-01 09:15:52 -0400

    Add piece methods

---
+++ lib/pieces.rb
@@ -0,0 +1,55 @@
+module Pieces
+	def empty 
+		"   "
+	end
+
+	def w_king
+		" \u265A "
+	end
+
+	def b_king
+		" \u2654 "
+	end
+
+	def w_queen
+		" \u265B "
+	end
+
+	def b_queen
+		" \u2655 "
+	end
+
+	def w_rook
+		" \u265C "
+	end
+
+	def b_rook
+		" \u2656 "
+	end
+
+	def w_bishop
+		" \u265D "
+	end
+
+	def b_bishop
+		" \u2657 "
+	end
+
+	def w_knight
+		" \u265E "
+	end
+
+	def b_knight
+		" \u2658 "
+	end
+
+	def w_pawn
+		" \u265F "
+	end
+
+	def b_pawn
+		" \u2659 "
+	end
+end
+
+

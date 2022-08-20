855d5c8e4202c572ff3b91eb67bbf5563c9153d1
Brent Steffen <BESteffen82@gmail.com>
2022-08-20 15:47:19 -0400

    Deleted the file from the git repository

--- lib/piece_reference.rb
+++
@@ -1,7 +0,0 @@
-require_relative "pieces/empty"
-require_relative "pieces/king"
-require_relative "pieces/queen"
-require_relative "pieces/rook"
-require_relative "pieces/bishop"
-require_relative "pieces/knight"
-require_relative "pieces/pawn"
